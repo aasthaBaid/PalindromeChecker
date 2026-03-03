415ba2048a956be6802bfd5e682c368d7decf640
aasthaBaid <aasthabaid3@gmail.com>
2026-03-03 16:01:06 +0530

    Application entry and welcome message

---
+++ src/com/palindrome/Welcome.java
@@ -0,0 +1,16 @@
+package com.palindrome;
+/* Application entry and Welcome message
+ * entry point
+ * @author developer
+ * @version 1.0
+ * */
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// Application name is displayed
+		System.out.println("Welcome to Palindrom checker app.\nVersion 1.0");
+		System.out.println("System Initiated Successfully!");
+	}
+
+}
